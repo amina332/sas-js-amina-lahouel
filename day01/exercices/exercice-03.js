6ad7a30e9fdb49177db0f92cd4db7b117eedc5a2
lahouel amina <aminalahouel9@gmail.com>
2026-09-08 12:53:17 +0200

    terminerexercice3

--- day01/exercices/exercice-03.js
+++ day01/exercices/exercice-03.js
@@ -17,4 +17,9 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+const HT = 200
+const TVA = 0.20
+const TTC = (HT + (HT * TVA))
+console.log(TTC);
+
