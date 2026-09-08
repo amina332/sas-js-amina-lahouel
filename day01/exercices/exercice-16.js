4019886068eb4af2f04a400f95a0f300bea45629
lahouel amina <aminalahouel9@gmail.com>
2026-09-08 17:20:50 +0200

    terminerexercice16

--- day01/exercices/exercice-16.js
+++ day01/exercices/exercice-16.js
@@ -14,4 +14,11 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+
+const numero = "0612345678";
+
+if (typeof numero === "string" && numero.length === 10) 
+{console.log(numero);}
+else {console.log("Numéro invalide");}
