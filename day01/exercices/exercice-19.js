54626e194183b008caa4b4fa5adc24866823c12c
lahouel amina <aminalahouel9@gmail.com>
2026-09-08 17:30:07 +0200

    terminerexercice19

--- day01/exercices/exercice-19.js
+++ day01/exercices/exercice-19.js
@@ -18,4 +18,15 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+
+
+const hasAccount = false;
+const isGuest = true;
+const cartValue = 150;
+const isBanned = false;
+
+if (!isBanned && (hasAccount || (isGuest && cartValue > 100)))
+{console.log("Commande validée");}
+else {console.log("Commande refusée");}
