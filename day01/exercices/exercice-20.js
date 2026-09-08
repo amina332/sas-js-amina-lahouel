4539c3c042587b5e158bf75563cae714cf86ba8a
lahouel amina <aminalahouel9@gmail.com>
2026-09-08 17:31:27 +0200

    terminerexercice20

--- day01/exercices/exercice-20.js
+++ day01/exercices/exercice-20.js
@@ -17,4 +17,12 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+
+const nom = null;
+const pseudo = "NinjaCoder";
+
+const nomAAfficher = nom ?? pseudo ?? "Anonyme";
+
+console.log(nomAAfficher);
