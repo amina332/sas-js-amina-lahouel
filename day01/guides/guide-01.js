f43b87654188f4f1aaee6279b0dd5d807e14e2e8
lahouel amina <aminalahouel9@gmail.com>
2026-09-08 17:35:12 +0200

    terminerguide01

--- day01/guides/guide-01.js
+++ day01/guides/guide-01.js
@@ -9,4 +9,16 @@
  */
 'use strict';
 
-// TODO: écris ta solution ici.
+.
+
+'use strict';
+
+let a = 5;
+let b = 10;
+
+let temporaire = a;
+a = b;
+b = temporaire;
+
+console.log(a);
+console.log(b);
