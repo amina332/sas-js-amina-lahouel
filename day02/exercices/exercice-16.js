ce45f8d698d0fa17658665ef8f0c1abc1130fcb7
lahouel amina <aminalahouel9@gmail.com>
2026-09-10 17:52:42 +0200

    jour 02 : terminer les exercices 01 a 07

--- day02/exercices/exercice-16.js
+++ day02/exercices/exercice-16.js
@@ -21,3 +21,19 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 
+
+for(let ligne = 1; ligne <= 5; ligne++) {
+    let espaces = "";
+    let etoiles = "";
+
+    for (let i = 1; i <= 5 - ligne; i++) {
+        espaces = espaces + " ";
+    }
+
+    for (let i = 1; i <= 2 * ligne - 1; i++) {
+        etoiles = etoiles + "*";
+    }
+
+    console.log(espaces + etoiles);
+
+}
