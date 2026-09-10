ce93b962878f7e0b2bf967dc4786cf452e78cc49
lahouel amina <aminalahouel9@gmail.com>
2026-09-10 15:01:47 +0200

    jour 02 : terminer les exercices 01 a 07

--- day02/exercices/exercice-13.js
+++ day02/exercices/exercice-13.js
@@ -15,12 +15,14 @@
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
 
+let x = 0
+let y = 1
+for(let i=0; i < 10; i++){
+    console.log(x)
+    let w = x + y
+    x=y; 
+    x=i
+}
 
 
 
-let num1 = 0 ;
-let num2 =1 ;
-console.log(num1)
-console.log(num2)
-
-for(let i=2 ; i<=10 ; i++){let fact = num1+num2; console.log(fqct);num1;1=num2;num1=fact}
