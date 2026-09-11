b7203de12c1da1feff3245d2c1efa5d530c61e9e
lahouel amina <aminalahouel9@gmail.com>
2026-09-11 15:20:26 +0200

    jour 04 : terminer les exercices 01 a 07

--- day04/exercices/exercice-13.js
+++ day04/exercices/exercice-13.js
@@ -13,5 +13,19 @@
 'use strict';
 
 // 1. Identifie les données nécessaires.
-// 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+// 2. Écris ta solution sous cette ligne
+
+
+function genererMotDePasse(longueur){
+    let motDePasse = ""
+
+    for (let i = 0; i < longueur; i++){
+        if (Math.random() < 0.5){
+            motDePasse += "A"
+        }else{
+            motDePasse += Math.floor(Math.random() * 10)
+        }
+}
+return motDePasse
+ } 
+console.log(genererMotDePasse(6));
