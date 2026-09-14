95a6695f5327999781c15ccfc30729d9c0ce4507
lahouel amina <aminalahouel9@gmail.com>
2026-09-14 15:17:28 +0200

    jour 05 : terminer les exercices 01 a 07

--- day05/exercices/exercice-10.js
+++ day05/exercices/exercice-10.js
@@ -14,4 +14,11 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+
+function adresse (email) {
+return email.split("@")[1]
+}
+console.log(adresse ("contact@youcode.ma"));
+
