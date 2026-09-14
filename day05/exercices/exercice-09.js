fdd2a90df0f15fa4424feb6ed3889519ea5e05c8
lahouel amina <aminalahouel9@gmail.com>
2026-09-14 13:28:22 +0200

    jour 05 : terminer les exercices 01 a 07

--- day05/exercices/exercice-09.js
+++ day05/exercices/exercice-09.js
@@ -14,4 +14,9 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+let phrase = "Ne sois pas idiot, réfléchis avant de parler."
+phrase = phrase.replace("idiot" , "*")
+console.log(phrase);
+
