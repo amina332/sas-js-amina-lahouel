af38a67e51b079b8de6467d61c6895f0c9108c98
lahouel amina <aminalahouel9@gmail.com>
2026-09-14 12:01:21 +0200

    jour 05 : terminer les exercices 01 a 07

--- day05/exercices/exercice-03.js
+++ day05/exercices/exercice-03.js
@@ -7,11 +7,22 @@
  * 🎯 MISSION
  * Mettez "youcode" entièrement en majuscules, et "MAROC" entièrement en minuscules.
  *
- * 📖 Consigne détaillée : ../03-exercices.md#exercice-03
+ * 📖 Consigne détaillée : 
+ * ../03-exercices.md#exercice-03
  * ▶️ Commande : node day05/exercices/exercice-03.js
  */
 'use strict';
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+
+let m1 = "youcode"
+let m2 =  "MAROC"
+
+
+
+console.log(m1.toUpperCase() );
+console.log(m2.toLowerCase() )
+
