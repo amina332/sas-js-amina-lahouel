3c3566024c0a39eb24d3d7f03b47433f2da6868b
lahouel amina <aminalahouel9@gmail.com>
2026-09-15 15:03:42 +0200

    jour 06 : terminer les exercices 01 a 07

--- day06/exercices/exercice-14.js
+++ day06/exercices/exercice-14.js
@@ -14,4 +14,10 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let nombres =[1, 2, 3] 
+let dernier = nombres.pop();
+
+nombres.unshift(dernier);
+
+console.log(nombres);
