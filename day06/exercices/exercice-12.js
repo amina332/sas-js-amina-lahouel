c17ba1776038843b23686ca15cdaa56b638bfa56
lahouel amina <aminalahouel9@gmail.com>
2026-09-15 12:28:21 +0200

    jour 06 : terminer les exercices 01 a 07

--- day06/exercices/exercice-12.js
+++ day06/exercices/exercice-12.js
@@ -14,4 +14,12 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+function  doublement (tableau){
+
+tableau.splice(1,1)
+tableau.splice(4,1)
+return tableau
+}
+let nombres = [1, 2, 2, 3, 4, 4, 5]
+console.log(doublement(nombres));
