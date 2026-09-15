1031ef6f28afe23cc59153fdbba936749339a65a
lahouel amina <aminalahouel9@gmail.com>
2026-09-15 10:32:09 +0200

    jour 06 : terminer les exercices 01 a 07

--- day06/exercices/exercice-04.js
+++ day06/exercices/exercice-04.js
@@ -14,4 +14,11 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+
+
+let nombres = [10, 20, 30, 40]
+for (let i =0; i < nombres.length; i++ ){
+    console.log(nombres[i]*2);
+    
+}
