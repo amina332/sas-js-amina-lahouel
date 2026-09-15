39fc7c025a12a1d21c03d3b382b8e65bc5b4dc8a
lahouel amina <aminalahouel9@gmail.com>
2026-09-15 10:07:14 +0200

    jour 06 : terminer les exercices 01 a 07

--- day06/exercices/exercice-02.js
+++ day06/exercices/exercice-02.js
@@ -4,8 +4,8 @@
  * AJOUT DYNAMIQUE
  * ─────────────────────────────────────────────────────────────
  *
- * 🎯 MISSION
- * Créez un tableau vide invites = []. Ajoutez-y successivement "Yassine", "Sara", puis "Amine" en utilisant des méthodes de tableau.
+ * 🎯 MISSION"Yassine", "Sara", puis "Amine"
+ *. Ajoutez-y successivement  en utilisant des méthodes de tableau.
  *
  * 📖 Consigne détaillée : ../03-exercices.md#exercice-02
  * ▶️ Commande : node day05/exercices/exercice-02.js
@@ -14,4 +14,4 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
