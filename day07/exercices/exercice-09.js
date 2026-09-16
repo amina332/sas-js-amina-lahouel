4e9cf565a36bb483663ecba447d3ebdf728c1881
lahouel amina <aminalahouel9@gmail.com>
2026-09-16 13:29:32 +0200

    jour 07 : terminer les exercices 01 a 07

--- day07/exercices/exercice-09.js
+++ day07/exercices/exercice-09.js
@@ -14,4 +14,29 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let candidats = [{
+    nom : "wiam" , score :9,
+},
+
+{
+    nom : "khalid" , score : 12 ,
+},
+
+{
+    nom : "zineb" , score : 5
+
+}
+
+]
+function admit (candidats) {
+    let resultat = []
+    for (let i = 0; i < candidats.length; i++) {
+       if (candidats[i].score >= 10 ){
+        resultat.push = candidats[i]
+       }
+        
+    }
+    return resultat
+}
+console.log(admit (candidats));
