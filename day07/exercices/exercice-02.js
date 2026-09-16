f2bc3598565defbb03ea3fb35448b241d6cef288
lahouel amina <aminalahouel9@gmail.com>
2026-09-16 09:57:00 +0200

    jour 07 : terminer les exercices 01 a 07

--- day07/exercices/exercice-02.js
+++ day07/exercices/exercice-02.js
@@ -14,4 +14,12 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+
+let utilisateur = {
+    nom : "Amina",
+    age : 18 ,
+    Admin : false,
+}
+utilisateur.age = utilisateur.age + 1
+utilisateur.email = "aminalahouel9@gmail.com"
+console.log(utilisateur);
